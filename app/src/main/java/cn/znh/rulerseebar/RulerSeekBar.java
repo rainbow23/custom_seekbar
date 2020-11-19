784273ccc03c2e12f29215109cbb65cca540d9ff
rainbow23 <info@rainbow23.jp>
2020-11-19 09:35:57 +0900

    clean clode

--- app/src/main/java/cn/znh/rulerseebar/RulerSeekBar.java
+++ app/src/main/java/cn/znh/rulerseebar/RulerSeekBar.java
@@ -43,71 +43,26 @@ public class RulerSeekBar extends AppCompatSeekBar {
      */
     private int mRulerColor = Color.WHITE;
 
-    Bitmap bmp;
-
-    /**
-     * 滑块上面是否要显示刻度线
-     */
-    private boolean isShowTopOfThumb = false;
-
     public RulerSeekBar(Context context) {
         super(context);
-        this.context = context;
         init();
     }
 
     public RulerSeekBar(Context context, AttributeSet attrs) {
         super(context, attrs);
-       this.context = context;
         init();
     }
 
     public RulerSeekBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        this.context = context;
         init();
     }
 
-private Context context = null;
-private Drawable drawable = null;
-
-    /**
-     * 初始化
-     */
     private void init() {
         //创建绘制刻度线的画笔
         mRulerPaint = new Paint();
         mRulerPaint.setColor(mRulerColor);
         mRulerPaint.setAntiAlias(true);
-
-        Resources resources = this.context.getResources();
-
-        if(resources == null) {
-            Log.d("main", "resources == null");
-        }else{
-            Log.d("main", "resources != null");
-
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inScaled = false;
-
-            bmp = BitmapFactory.decodeResource(resources, R.drawable.shape_progress_drawable, options);
-//            drawable = this.context.getDrawable(R.drawable.shape_thumb_icon);
-
-        }
-
-
-
-
-        if(bmp != null) {
-            Log.d("main", "bmp != null");
-        }else {
-            Log.d("main", "bmp == null");
-        }
-        if(drawable != null) {
-            Log.d("main", "drawable != null");
-        }else {
-            Log.d("main", "drawable == null");
-        }
 
         //Api21及以上调用，去掉滑块后面的背景
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -121,109 +76,27 @@ private Drawable drawable = null;
         mPosX = posX;
     }
 
-    /**
-     * 重写onDraw方法绘制刻度线
-     *
-     * @param canvas
-     */
     @Override
+    public synchronized void setProgress(int progress) {
+
+    }
+        @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        //极限条件校验
-        if (getWidth() <= 0 || mRulerCount <= 0) {
-            return;
-        }
-
-        //获取每一份的长度
-        int length = (getWidth() - getPaddingLeft() - getPaddingRight() - mRulerCount * mRulerWidth) / (mRulerCount + 1);
-
-        //计算刻度线的顶部坐标和底部坐标
-        int rulerTop = getHeight() / 2 - getMinimumHeight() / 2;
-        int rulerBottom = rulerTop + getMinimumHeight();
-
-        //获取滑块的位置信息
-        Rect thumbRect = null;
-        if (getThumb() != null) {
-            thumbRect = getThumb().getBounds();
-        }
-
-        //绘制刻度线
-        for (int i = 1; i <= mRulerCount; i++) {
-            //计算刻度线的左边坐标和右边坐标
-            int rulerLeft = i * length + getPaddingLeft();
-            int rulerRight = rulerLeft + mRulerWidth;
-
-            //判断是否需要绘制刻度线
-            if (!isShowTopOfThumb && thumbRect != null && rulerLeft - getPaddingLeft() > thumbRect.left && rulerRight - getPaddingLeft() < thumbRect.right) {
-                continue;
-            }
-
-//    drawable.draw(canvas);
-
-//            Bitmap bmp = BitmapFactory.decodeResource(this.getContext().getResources(), R.drawable.shape_thumb_icon);
-//            if(bmp != null) {
-//                canvas.drawBitmap(bmp, 10, 10, mRulerPaint);
-//            }
-
-//            canvas.drawCircle(200f,
-//                    getThumb().getBounds().centerY(),
-//                    (rulerBottom - rulerTop) * 0.7f, mRulerPaint);
-            //进行绘制
-            canvas.drawRect(rulerLeft, rulerTop, rulerRight, rulerBottom, mRulerPaint);
-        }
-
-        final float width=getMeasuredWidth()-getPaddingLeft()-getPaddingRight();
-        final float step=width/(float)(getMax());
-
-        float thumbPositionX = width
-                * (float) getProgress()
-                / (float) getMax()
-                + (float) getPaddingLeft();
-
-        Log.d("Ruler",
-              "mPosX = " + mPosX +
-                    " mDotRadius = " + mDotRadius
-        );
-
-        float moveXSpace = (float)getMaxHeight() - (float)mDotRadius
-                * (float) getProgress()
-                / (float) getMax();
-
-        thumbPositionX += mDotRadius/2;
-        //radius = 半径
-        float halfPositionX = (float)getMax()/2;
-        if(mPosX < halfPositionX) {
-            float percentX = (halfPositionX - (float)mPosX) / halfPositionX;
-            float moveX   = percentX * moveXSpace; //ずらす値
-            mPosX         += moveX;
-
-            Log.d("Ruler",
-                    "halfPositionX = " + halfPositionX +
-                            " percentX = " + percentX +
-                            " moveXSpace = " + moveXSpace +
-                            " moveX = " + moveX);
-//            mPosX += mDotRadius;  //左位置をthumbと同じにする
-//            mPosX += getMaxHeight()/2;
-        }else{
-//            mPosX -= mDotRadius;  //右位置をthumbと同じにする
-//            mPosX -= getMaxHeight()/2;
-        }
-
-        //canvas.drawCircle(thumbPositionX,//mPosX * step,
         canvas.drawCircle(
                 getThumb().getBounds().centerX(),
                 getThumb().getBounds().centerY(),
                 mDotRadius,
                 mRulerPaint);
-        Log.d("Ruler",
-        "getMax() = " + getMax() +
-         "  getMaxHeight() = " +  getMaxHeight() +
-         "  getHeight() = " +  getHeight() +
-         "  getProgress() = " +  getProgress() +
-         "  width = " + width +
-         "  step = " + step +
-         "  thumbPositionX = " +  thumbPositionX
+//        Log.d("Ruler",
+//        "getMax() = " + getMax() +
+//         "  getMaxHeight() = " +  getMaxHeight() +
+//         "  getHeight() = " +  getHeight() +
+//         "  getProgress() = " +  getProgress() +
+//         "  width = " + width +
+//         "  step = " + step +
+//         "  thumbPositionX = " +  thumbPositionX
 
         );
 
